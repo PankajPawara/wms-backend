f9b68969a992d7c1cb61cb34bffd3382c3296b95
PankajPawara <pankajpawara1810@gmail.com>
2026-07-13 22:51:54 +0530

    chore: Remove hardcoded MongoDB credentials to resolve secret exposure alert

--- reset_pass.js
+++ reset_pass.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-mongoose.connect('mongodb+srv://pankajpawara1810_db_user:vfYWWTBy6AhUB8Ks@wms-cluster.dvugvah.mongodb.net/?appName=WMS-Cluster').then(async () => {
+mongoose.connect(process.env.MONGODB_URI).then(async () => {
   const db = mongoose.connection.db;
   const hash = await bcrypt.hash('Pass@1234', 12);
   await db.collection('users').updateOne({employee_id: '15'}, {$set: {password_hash: hash}});
